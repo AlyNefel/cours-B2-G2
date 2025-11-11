c58721fd7d5cc63601bd4416cffe6f41acbb3e82
alynefel <ali.nefel2020@gmail.com>
2025-11-11 16:25:18 +0000

    js intro

---
+++ javascript/variables&intro/script.js
@@ -0,0 +1,15 @@
+var name ="Molka"
+//  console log to print 
+console.log(name)
+
+// condition 
+var age =22
+
+if(age>18){
+    // <p>welcome to the party</p>
+    var reponse = document.getElementById("response")
+    reponse.value="Welcome to the party !!"
+console.log("Welcome to the party !!")
+} else {
+console.log("Drink some Milk and sleep !! ")
+}
